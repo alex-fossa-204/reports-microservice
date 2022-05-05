bd3809d09e168a4f0205df6f7c665125627c4555
nexus <shift.kite11@yandex.ru>
2022-05-05 11:31:20 +0300

    [add]: utils and props implementation

--- src/main/java/by/andersen/laboratory/microservice/reports/utils/RandomUidGenerator.java
+++ src/main/java/by/andersen/laboratory/microservice/reports/utils/RandomUidGenerator.java
@@ -1,4 +1,14 @@
 package by.andersen.laboratory.microservice.reports.utils;
 
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
 public class RandomUidGenerator {
+
+    public Long generateRandomTransferUid() {
+        return UUID.randomUUID().getMostSignificantBits() >>> 16;
+    }
+
 }
