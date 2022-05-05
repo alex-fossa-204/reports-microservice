866b2908ae4eba24d52705790343feaa15483727
nexus <shift.kite11@yandex.ru>
2022-05-05 11:29:43 +0300

    [add]: repositories implementaion

--- src/main/java/by/andersen/laboratory/microservice/reports/repository/ReportRepository.java
+++ src/main/java/by/andersen/laboratory/microservice/reports/repository/ReportRepository.java
@@ -1,2 +1,17 @@
-package by.andersen.laboratory.microservice.reports.repository;public interface ReportRepository {
+package by.andersen.laboratory.microservice.reports.repository;
+
+import by.andersen.laboratory.microservice.reports.model.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    List<Report> findAllByReporterUid(Long reporterUid);
+
+    Optional<Report> findByUid(Long uid);
+
 }
--- src/main/java/by/andersen/laboratory/microservice/reports/repository/ReportStatusRepository.java
+++ src/main/java/by/andersen/laboratory/microservice/reports/repository/ReportStatusRepository.java
@@ -1,2 +1,12 @@
-package by.andersen.laboratory.microservice.reports.repository;public interface ReportStatusRepository {
+package by.andersen.laboratory.microservice.reports.repository;
+
+import by.andersen.laboratory.microservice.reports.model.ReportStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportStatusRepository extends JpaRepository<ReportStatus, Long> {
+
+    ReportStatus findByReportStatusName(String reportStatusName);
+
 }
