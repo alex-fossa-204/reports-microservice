74e1f4ad8a035961db30434b1b0b803cf7b830b4
nexus <shift.kite11@yandex.ru>
2022-05-05 11:33:56 +0300

    [edit]: test fixes

--- src/test/java/by/andersen/laboratory/microservice/reports/repository/ReportRepositoryTest.java
+++ src/test/java/by/andersen/laboratory/microservice/reports/repository/ReportRepositoryTest.java
@@ -1,7 +1,6 @@
 package by.andersen.laboratory.microservice.reports.repository;
 
 import by.andersen.laboratory.microservice.reports.model.Report;
-import by.andersen.laboratory.microservice.reports.model.ReportStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +14,6 @@ public class ReportRepositoryTest {
     @Autowired
     private ReportRepository reportRepository;
 
-    @Autowired
-    private ReportStatusRepository reportStatusRepository;
-
     @Test
     public void findAllReports() {
         List<Report> reports = reportRepository.findAll();
@@ -30,12 +26,6 @@ public class ReportRepositoryTest {
         List<Report> reports = reportRepository.findAllByReporterUid(786486841L);
         Assertions.assertNotNull(reports);
         reports.forEach(System.out::println);
-    }
-
-    @Test
-    public void findReportStatusName() {
-        ReportStatus reportStatus = reportStatusRepository.findByReportStatusName("STARTED");
-        System.out.println(reportStatus);
     }
 
 }
