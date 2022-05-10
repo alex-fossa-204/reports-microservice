179b3c5976348d06a5b429a28061becb36c11d21
nexus <shift.kite11@yandex.ru>
2022-05-10 12:03:12 +0300

    [add]: few code cleanup

--- src/main/resources/db/changelog/changeset-populate_table/reports-tables-population-script-reports.sql
+++ src/main/resources/db/changelog/changeset-populate_table/reports-tables-population-script-reports.sql
@@ -2,8 +2,10 @@
 
 --changeset Bakulin:reports_table_population splitStatements:true endDelimeter:;
 
-INSERT INTO report OVERRIDING SYSTEM VALUE VALUES (2, 'Initialized microservice database', 'Initialized microservice database for reports', '2022-05-03 00:00:00', 1, 786784177, 2, NULL, 314235235);
-INSERT INTO report OVERRIDING SYSTEM VALUE VALUES (3, 'Initialized Java Project', 'Initialized Spring Boot Project for users', '2022-05-03 00:00:00', 1, 786786787, 2, NULL, 1354235125);
-INSERT INTO report OVERRIDING SYSTEM VALUE VALUES (4, 'Initialized Java project', 'Initialized Spring Boot Project for reports', '2022-05-03 00:00:00', 1, 786786877, 2, NULL, 32543254325);
-INSERT INTO report OVERRIDING SYSTEM VALUE VALUES (5, 'Add users service implementaion', 'Add users service implementaion, repositroy, controller, entities and etc', '2022-05-03 00:00:00', 1, 786486841, 2, NULL, 3344524542);
-INSERT INTO report OVERRIDING SYSTEM VALUE VALUES (1, 'Added database schema', 'Initialized microservice db for users, and test update', '2022-05-03 00:00:00', 1, 786486841, 2.5, '2022-05-05 10:02:23.984', 12312343);
+INSERT INTO report (report_title, report_body, report_date, report_status, reporter_uid, time_cost, update_date, uid)
+VALUES
+('Initialized microservice database', 'Initialized microservice database for reports', '2022-05-03 00:00:00', 1, 786784177, 2, NULL, 314235235),
+('Initialized Java Project', 'Initialized Spring Boot Project for users', '2022-05-03 00:00:00', 1, 786786787, 2, NULL, 1354235125),
+('Initialized Java project', 'Initialized Spring Boot Project for reports', '2022-05-03 00:00:00', 1, 786786877, 2, NULL, 32543254325),
+('Add users service implementaion', 'Add users service implementaion, repositroy, controller, entities and etc', '2022-05-03 00:00:00', 1, 786486841, 2, NULL, 3344524542),
+('Added database schema', 'Initialized microservice db for users, and test update', '2022-05-03 00:00:00', 1, 786486841, 2.5, '2022-05-05 10:02:23.984', 12312343);
--- src/main/resources/db/changelog/changeset-populate_table/reports-tables-population-script-reports_status.sql
+++ src/main/resources/db/changelog/changeset-populate_table/reports-tables-population-script-reports_status.sql
@@ -2,5 +2,7 @@
 
 --changeset Bakulin:reports_statuses_inserts splitStatements:true endDelimeter:;
 
-INSERT INTO report_status OVERRIDING SYSTEM VALUE VALUES (1, 'STARTED');
-INSERT INTO report_status OVERRIDING SYSTEM VALUE VALUES (2, 'FINISHED');
+INSERT INTO report_status (report_status_name)
+VALUES
+('STARTED'),
+('FINISHED');
